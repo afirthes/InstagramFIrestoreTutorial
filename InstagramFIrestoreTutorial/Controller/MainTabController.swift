c05617080ad7f1ddffef0b9d233f01a3b15a7f42
afirthes <afirthes@gmail.com>
2024-06-22 12:04:02 +0300

    Initial commit

--- InstagramFIrestoreTutorial/Controller/MainTabController.swift
+++ InstagramFIrestoreTutorial/Controller/MainTabController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainTabController.swift
 //  InstagramFIrestoreTutorial
 //
 //  Created by Icetusk on 22.06.2024.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class MainTabController: UITabBarController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.backgroundColor = .red
     }
-
-
 }
-
