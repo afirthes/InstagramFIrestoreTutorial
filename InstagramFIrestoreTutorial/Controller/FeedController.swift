27220850f9b47bdf470dc1a29e42fcbacafdd7c8
afirthes <afirthes@gmail.com>
2024-06-22 22:30:14 +0300

    Finished post list at first glance

--- InstagramFIrestoreTutorial/Controller/FeedController.swift
+++ InstagramFIrestoreTutorial/Controller/FeedController.swift
@@ -7,11 +7,50 @@
 
 import UIKit
 
-class FeedController: UIViewController {
+private let reuseIndentifier = "Cell"
+
+class FeedController: UICollectionViewController {
+    
+    // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: - Helpers
+    
+    func configureUI() {
+        collectionView.backgroundColor = .white
+        collectionView.register(FeedCell.self, forCellWithReuseIdentifier: reuseIndentifier)
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+
+extension FeedController {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIndentifier, for: indexPath) as! FeedCell
         
-        view.backgroundColor = .systemBlue
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+
+extension FeedController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let width = view.frame.width
+        var height =  width + 8 + 40 + 8
+        height += 50 // image
+        height += 60 // all other stuf
+         
+        return CGSize(width: width, height: height)
+        
     }
 }
--- InstagramFIrestoreTutorial/Controller/MainTabController.swift
+++ InstagramFIrestoreTutorial/Controller/MainTabController.swift
@@ -13,44 +13,27 @@ class MainTabController: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureTabBarStyle()
         configureViewControllers()
     }
     
     // MARK: - Helpers
     
-    func configureTabBarStyle() {
-        if #available(iOS 13.0, *) {
-            let appearance = UINavigationBarAppearance()
-            appearance.configureWithOpaqueBackground()
-            appearance.backgroundColor = .blue
-            appearance.titleTextAttributes = [
-                NSAttributedString.Key.font: UIFont.systemFont(ofSize: 20),
-                NSAttributedString.Key.foregroundColor: UIColor.white
-            ]
-            appearance.largeTitleTextAttributes = [
-                NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 34),
-                NSAttributedString.Key.foregroundColor: UIColor.white
-            ]
-            
-            navigationController?.navigationBar.standardAppearance = appearance
-            navigationController?.navigationBar.scrollEdgeAppearance = appearance
-        }
-    }
-    
     func configureViewControllers() {
         
         view.backgroundColor = .white
         
-        let feed = templateNavigationController("home_unselected", "home_selected", FeedController())
+        let  layout = UICollectionViewFlowLayout()
+        
+        let feed = templateNavigationController("home_unselected", "home_selected", FeedController(collectionViewLayout: layout))
         let search = templateNavigationController("search_unselected", "search_selected", SearchController())
         let imageSelector = templateNavigationController("plus_unselected", "plus_unselected", ImageSelectorController())
         let notifications = templateNavigationController("like_unselected", "line_selected", NotificationController())
         let profile = templateNavigationController("profile_unselected", "profile_selected", ProfileController())
         
-        viewControllers = [feed, search, imageSelector, notifications, profile]
+        self.viewControllers = [feed, search, imageSelector, notifications, profile]
         
         tabBar.isTranslucent = false
+        tabBar.tintColor = .black
     }
     
     func templateNavigationController(_ unselectedImage: String, _ selectedImage: String, _ rootViewController: UIViewController) -> UINavigationController {
